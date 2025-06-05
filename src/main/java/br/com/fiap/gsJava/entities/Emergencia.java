8e096e3b99d2e65c5eebf489c29abbb9e8ed5b9b
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-06-04 23:52:17 -0300

    Criação da classe Emergencia

---
+++ src/main/java/br/com/fiap/gsJava/entities/Emergencia.java
@@ -0,0 +1,28 @@
+package br.com.fiap.gsJava.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_emergencia")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emergencia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String mensagem;
+	private Boolean status;
+	
+	
+}
